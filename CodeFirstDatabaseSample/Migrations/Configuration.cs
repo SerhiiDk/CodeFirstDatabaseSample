7d75957de9f6656ad00ee268d735a9ca9f311651
Serhii Voloshchuk <serh0227@edu.sde.dk>
2022-05-16 13:13:30 +0200

    added migrations

---
+++ CodeFirstDatabaseSample/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace CodeFirstDatabaseSample.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirstDatabaseSample.Program.BloggingContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "CodeFirstDatabaseSample.Program+BloggingContext";
+        }
+
+        protected override void Seed(CodeFirstDatabaseSample.Program.BloggingContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
